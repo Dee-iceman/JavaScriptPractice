425bcbd40b4a94cbfabc744a580bf64f80276466
manuel <manueluyanguren05@gmail.com>
2025-12-16 20:50:37 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//alert ('Hello Manuel');
+
+let FullName = 'Manuel B. Uyanguren';
+let Age = 26;
+let Birthday = '09/01/99';
+let Address = 'Farland Extension'; 
+let message = 'Hello World!';
+
+/*
+console.log(FullName + '\n' + Age + '\n' + Birthday + '\n'+ Address);
+console.log(message);
+*/
+
+/*
+console.log
+(`
+FullName: ${FullName} 
+Age: ${Age}
+Birthday: ${Birthday}
+Address: ${Address}
+`);
+*/
+
+alert (`
+FullName: ${FullName} 
+Age: ${Age}
+Birthday: ${Birthday}
+Address: ${Address}
+`);
